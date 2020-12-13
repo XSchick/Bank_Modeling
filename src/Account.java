b177731834f90ef0599e1bbe2e0e1e04ec3a2850
Xaver Schick <Xaver.schick@web.de>
2020-12-13 13:11:13 +0100

    Adding constructor in class account

--- src/Account.java
+++ src/Account.java
@@ -5,6 +5,12 @@ public class Account {
     int bankCode;
     int balance;
 
+    public Account(int theBankCode, int theBankNumber){
+        this.bankCode = theBankCode;
+        this.accountNumber = theBankNumber;
+        this.balance = 0;
+    }
+
     //public boolean withdraw(int amount) {}
     //public void deposit(int amount) {}
     //public boolean transfer(Account  account ,int amount)
